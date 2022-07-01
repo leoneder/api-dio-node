16dd5e2562820d4dff571b035bea4364a227863f
leoneder <leoneder@ymail.com>
2022-07-01 13:57:32 -0300

    iniciado 1/07

--- src/controllers/CreateUserController.test.ts
+++ src/controllers/CreateUserController.test.ts
@@ -1,8 +1,9 @@
+import { request, response } from "express";
 import { CreateUserController } from "./CreateUserController"
 
 describe('CreateUserController', () =>{
    it('Deve retornar o id', ()=>{
     const createUserController = new CreateUserController;
-    const result = createUserController.handle();
+    const result = createUserController.handle(request, response);
    })
 })
